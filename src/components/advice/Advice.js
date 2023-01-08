f0300559adbcf4cc37edb70c67a3287bc8814802
vibesx <lucian.paslariu@gmail.com>
2023-01-08 13:58:07 +0200

    added loading spinner and deactivated get advice button while fetching

--- src/components/advice/Advice.js
+++ src/components/advice/Advice.js
@@ -1,34 +1,88 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+
+import { useState } from "react";
 
 import classes from "./Advice.module.scss";
 import dividerDesktop from "../../assets/pattern-divider-desktop.svg";
 import dividerMobile from "../../assets/pattern-divider-mobile.svg";
 
+import useHttpRequest from "../hooks/use-http-request";
+
 import Card from "../ui/Card";
 import GetAdviceButton from "../ui/GetAdviceButton";
+import LoadingSpinner from "../ui/LoadingSpinner";
+
+const GET_ADVICE_URL = "https://api.adviceslip.com/advice";
 
 function Advice() {
-	const adviceText =
-		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ipsam ea fugit a, modi rem iusto unde quis autem sed?";
+	const [adviceId, setAdviceId] = useState("");
+	const [adviceText, setAdviceText] = useState("");
+	const { isLoading, httpError, sendRequest } = useHttpRequest();
+
+	const fetchDataAndPopulate = useCallback(async () => {
+		// console.log(isLoading);
+		const { slip } = await sendRequest(GET_ADVICE_URL);
+
+		setAdviceId(slip.id);
+		setAdviceText(slip.advice);
+	}, [sendRequest]);
+
+	const clickHandler = async () => {
+		if (isLoading) {
+			return;
+		}
+		await fetchDataAndPopulate();
+		console.log("finished fetching");
+	};
+
+	useEffect(() => {
+		const getAdvice = async () => {
+			await fetchDataAndPopulate();
+		};
+
+		try {
+			getAdvice();
+		} catch (error) {
+			console.error(error.message);
+		}
+	}, [sendRequest, fetchDataAndPopulate]);
+
+	if (httpError) {
+		return (
+			<Card>
+				<section>
+					<p>{httpError}</p>
+				</section>
+			</Card>
+		);
+	}
+
 	return (
 		<Card>
-			<div className={classes.adviceContainer}>
-				<h6>ADVICE #117</h6>
-				<p>&#8220;{`${adviceText}`}&#8221;</p>
-				<picture className={classes.separator}>
-					<source
-						media="(min-width: 376px)"
-						srcSet={dividerDesktop}
-					/>
-					<img
-						src={dividerMobile}
-						alt=""
-						aria-hidden="true"
-						className={classes.separator}
-					/>
-				</picture>
-			</div>
-			<GetAdviceButton></GetAdviceButton>
+			{isLoading ? (
+				<LoadingSpinner></LoadingSpinner>
+			) : (
+				<section className={classes.adviceContainer}>
+					<h6>{`ADVICE #${adviceId}`}</h6>
+					<p>&#8220;{`${adviceText}`}&#8221;</p>
+					<picture className={classes.separator}>
+						<source
+							media="(min-width: 376px)"
+							srcSet={dividerDesktop}
+						/>
+						<img
+							src={dividerMobile}
+							alt=""
+							aria-hidden="true"
+							className={classes.separator}
+						/>
+					</picture>
+				</section>
+			)}
+			<GetAdviceButton
+				onClickHandler={clickHandler}
+				isLoading={isLoading}
+			></GetAdviceButton>
 		</Card>
 	);
 }
--- src/components/ui/GetAdviceButton.js
+++ src/components/ui/GetAdviceButton.js
@@ -3,9 +3,14 @@ import React from "react";
 import classes from "./GetAdviceButton.module.scss";
 import iconDice from "../../assets/icon-dice.svg";
 
-function GetAdviceButton() {
+function GetAdviceButton(props) {
 	return (
-		<button className={classes.button}>
+		<button
+			className={`${classes.button} ${
+				props.isLoading ? classes.disabled : classes.enabled
+			}`}
+			onClick={props.onClickHandler}
+		>
 			<img src={iconDice} alt="Generate Advice" />
 		</button>
 	);
